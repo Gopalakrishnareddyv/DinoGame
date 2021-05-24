ff1d5257fca740b9e9f0fbac953f72bc70275885
GOPALAKRISHNAREDDY <gopalakrishna.vintha@xcubelabs.com>
2021-05-24 09:23:36 +0530

    DinoGame developed

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float jumpSpeed;
+    public float playerspeed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //JumpMovement();
+        this.GetComponent<Rigidbody>().velocity = new Vector3(playerspeed*Time.deltaTime, GetComponent<Rigidbody>().velocity.y, GetComponent<Rigidbody>().velocity.z);
+    }
+    private void OnCollisionStay(Collision collision)
+    {
+        //print(collision.gameObject.name);
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            JumpMovement();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.GetComponent<ObstacleController>()!=null)
+        {
+            Destroy(this.gameObject);
+            SceneManager.LoadScene(2);
+            // print("end");
+        }
+    }
+    private void JumpMovement()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            this.GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpSpeed, 0));
+            
+        }
+    }
+}
